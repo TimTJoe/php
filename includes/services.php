b6a0491226d495e613f05e269626ce8dde28c351
Timothy T. Joe <106974662+TimTJoe@users.noreply.github.com>
2025-03-19 17:53:07 +0000

    style services section

--- includes/services.php
+++ includes/services.php
@@ -1,3 +1,24 @@
 <div id="services" class="sized">
-  services
+  <header>
+    <h1>Our Services</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit fuga voluptatibus temporibus quas labore obcaecati praesentium iusto at molestiae, natus aut sunt minus quaerat nobis explicabo, adipisci corrupti reiciendis dolorum?</p>
+  </header>
+  <div class="grid">
+    <section class="cell">
+     <h3>THE NAME OF THE SERVICE HERE</h3>
+     <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam, itaque. Ad nemo aliquid quidem, earum neque consequuntur odio! Expedita corrupti maiores sequi nesciunt ex rem quos dolore sed voluptates veritatis.</span>
+    </section>
+    <section class="cell">
+     <h3>THE NAME OF THE SERVICE HERE</h3>
+     <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam, itaque. Ad nemo aliquid quidem, earum neque consequuntur odio! Expedita corrupti maiores sequi nesciunt ex rem quos dolore sed voluptates veritatis.</span>
+    </section>
+    <section class="cell">
+     <h3>THE NAME OF THE SERVICE HERE</h3>
+     <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam, itaque. Ad nemo aliquid quidem, earum neque consequuntur odio! Expedita corrupti maiores sequi nesciunt ex rem quos dolore sed voluptates veritatis.</span>
+    </section>
+    <section class="cell">
+     <h3>THE NAME OF THE SERVICE HERE</h3>
+     <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam, itaque. Ad nemo aliquid quidem, earum neque consequuntur odio! Expedita corrupti maiores sequi nesciunt ex rem quos dolore sed voluptates veritatis.</span>
+    </section>
+  </div>
 </div>
