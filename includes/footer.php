d7d4b2be1537e49fa52ec23d528b550017815099
Timothy T. Joe <106974662+TimTJoe@users.noreply.github.com>
2025-03-19 08:15:57 +0000

    updated footer and header

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<footer>
+        <p>&copy; <?= date('Y'); ?> My PHP Website</p>
+    </footer>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World Website</title>
+    <link rel="stylesheet" href="stylesheet/style.css">
+    <script src="javascript/jquery.js" defer></script>
+    <script src="javascript/script.js" defer></script>
+</head>
+<body>
+    <nav>
+      <div id="logo">
+        <img src="uploads/images/logo.jpg" alt="">
+      </div>
+      <div class="menu">
+        <a href="./">Home</a>
+        <a href="#section">Section</a>
+      </div>
+    </nav>
+
