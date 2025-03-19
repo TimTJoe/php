62cda89ad0289683ef6f31d5c767f471398deed5
Timothy T. Joe <106974662+TimTJoe@users.noreply.github.com>
2025-03-19 07:54:27 +0000

    update app config name

--- config/app.php
+++ config/app.php
@@ -1,7 +1,7 @@
 <?php
 return [
-    'app_name' => 'SRSCGroups.com',
-    'base_url' => 'http://localhost/srsc/public_html/',
+    'APP_NAME' => 'Good Old PHP App',
+    'BASE_URL' => 'http://localhost/php',
     'APP_ENV' => 'local',
     'APP_DEBUG' => 'true',
 ];
