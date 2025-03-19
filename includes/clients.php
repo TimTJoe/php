29561742c98a18d63e771d6e34ce3de01d25ed9d
Timothy T. Joe <106974662+TimTJoe@users.noreply.github.com>
2025-03-19 18:19:57 +0000

    add style for clients

--- includes/clients.php
+++ includes/clients.php
@@ -1,3 +1,35 @@
 <div id="our-clients" class="sized">
- 
+ <div class="left">
+  <h1>Who We Serve</h1>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque culpa qui repellat totam doloremque quas provident saepe vel. Vel ea, ullam voluptas et laboriosam animi molestiae itaque fuga ipsam molestias.</p>
+ </div>
+ <div class="right">
+  <a href="#">
+    <img src="uploads/images/image1.jpg" alt="">
+  </a>
+  <a href="#">
+    <img src="uploads/images/image1.jpg" alt="">
+  </a>
+  <a href="#">
+    <img src="uploads/images/image1.jpg" alt="">
+  </a>
+  <a href="#">
+    <img src="uploads/images/image1.jpg" alt="">
+  </a>
+  <a href="#">
+    <img src="uploads/images/image1.jpg" alt="">
+  </a>
+  <a href="#">
+    <img src="uploads/images/image1.jpg" alt="">
+  </a>
+  <a href="#">
+    <img src="uploads/images/image1.jpg" alt="">
+  </a>
+  <a href="#">
+    <img src="uploads/images/image1.jpg" alt="">
+  </a>
+  <a href="#">
+    <img src="uploads/images/image1.jpg" alt="">
+  </a>
+ </div>
 </div>
