304f9ebc49c0d2a67b0830d5c086def0297699d1
Timothy T. Joe <106974662+TimTJoe@users.noreply.github.com>
2025-03-20 12:48:40 +0000

    [SRSC] style contact section

--- includes/contact.php
+++ includes/contact.php
@@ -1,4 +1,12 @@
 <div id="contact-us" class="sized">
-  contact us
-
+  <div class="contacts">
+    <h1>Get in touch with us</h1>
+    <a href="mailto:info@srscgroup.com">info@srscgroup.com</a>
+    <a href="tel:231777000000"> +231 777 000 000</a>
+    <a href="tel:231888000000">+231 888 000 000</a>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias magnam qui ratione debitis ex vero, pariatur rerum sit. Fugit mollitia architecto beatae doloribus voluptatum maiores iure, quis repudiandae dignissimos illum?</p>
+  </div>
+  <div class="map">
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.632101514831!2d-10.802069226884242!3d6.311966625589243!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xf09f981bc4500cb%3A0xedff2cc315e88b05!2sNew%20Democrat!5e0!3m2!1sen!2s!4v1742474290599!5m2!1sen!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+  </div>
 </div>
