092c4a5bd288b732f60eee754508bc27c0302c95
Timothy T. Joe <106974662+TimTJoe@users.noreply.github.com>
2025-03-21 13:11:57 +0000

    [SRSC] Styled footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,43 @@
-<footer>
-        <p>&copy; <?= date('Y'); ?> My PHP Website</p>
-    </footer>
+<footer id="footer">
+    <div class="row flex">
+        <section class="story">
+            <img src="uploads/images/logo.jpg" alt="">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad eaque adipisci itaque sequi voluptas qui optio provident aliquid perferendis! Velit itaque maiores vel sed laudantium exercitationem quibusdam soluta ex voluptatibus.</p>
+        </section>
+
+        <section class="links">
+            <a href="#top">Home</a>
+            <a href="#services">Services</a>
+            <a href="#our-clients">Our Clients</a>
+            <a href="#trainings">Trainings</a>
+            <a href="#about-us">About Us</a>
+            <a href="#contact-us">Contact Us</a>
+        </section>
+
+        <section class="profiles">
+            <h3>Follow us</h3>
+            <a href="http://x.com/srscgroup" target="_blank" rel="noopener noreferrer" class="row">
+                <img src="uploads/images/facebook.svg" alt="">
+                <span>@srscgroup</span>
+            </a>
+            <a href="http://facebook.com/srscgroup" target="_blank" rel="noopener noreferrer" class="row">
+                <img src="uploads/images/x.svg" alt="">
+                <span>@srscgroup</span>
+            </a>
+            <a href="http://linkedin.com/company/srscgroup" target="_blank" rel="noopener noreferrer" class="row">
+                <img src="uploads/images/linkedin.svg" alt="">
+                <span>@srscgroup</span>
+            </a>
+        </section>
+
+    </div>
+    <div class="copyright">
+        <p>&copy; <?= date('Y'); ?> SRSC Group, Inc. Alright reserved.</p>
+        <p>
+            Developed by: <a href="http://facebook.com/timothytjoe" target="_blank" rel="noopener noreferrer">Timothy T. Joe</a>
+        </p>
+    </div>
+</footer>
 
 </body>
 </html>
