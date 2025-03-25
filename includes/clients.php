08448f3c2211c4435ab4f86f9a3c5abf57c49076
Timothy T. Joe <106974662+TimTJoe@users.noreply.github.com>
2025-03-25 15:53:00 +0000

    updated content

--- includes/clients.php
+++ includes/clients.php
@@ -1,41 +1,50 @@
 <div id="our-clients" class="sized">
  <div class="left">
   <header>
-    <h1>Who We Serve</h1>
+    <h1>Why Us?</h1>
   </header>
-  <p class="font-medium padding-top">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque culpa qui repellat totam doloremque quas provident saepe vel. Vel ea, ullam voluptas et laboriosam animi molestiae itaque fuga ipsam molestias. nesciunt!</p>
+  <p class="font-medium padding-top">SRSC was established with the core purpose of providing empirical evidence to inform policy for economic development in Liberia and the Mano River basin. Thus the firm is committed to providing quality and insightful research to aid government, international and local nongovernmental organizations, civil societies, and private institutions in formulating and analyzing policies and providing matrices and empirical findings for effective project management and evaluation. </p>
  </div>
  <div class="right">
   <header>
-    <h1>Our Partners</h1>
+    <h1>Who We Serve</h1>
   </header>
   <div class="rows">
-    <a href="#">
-      <img src="uploads/images/image1.jpg" alt="">
+    <a href="https://www.rti.org/" target="_blank">
+      <img src="uploads/images/RTI.webp" alt="">
     </a>
-    <a href="#">
-      <img src="uploads/images/image1.jpg" alt="">
+    <a href="https://www.moa.gov.lr/">
+      <img src="uploads/images/moa.svg" alt="">
     </a>
-    <a href="#">
-      <img src="uploads/images/image1.jpg" alt="">
+    <a href="https://practicalaction.org/">
+      <img src="uploads/images/pa.png" alt="">
     </a>
-    <a href="#">
-      <img src="uploads/images/image1.jpg" alt="">
+    <a href="https://www.eahealth.org/">
+      <img src="uploads/images/ea-health.png" alt="">
     </a>
-    <a href="#">
-      <img src="uploads/images/image1.jpg" alt="">
+    <a href="https://www.usaid.gov/">
+      <img src="uploads/images/usaid.png" alt="">
     </a>
-    <a href="#">
-      <img src="uploads/images/image1.jpg" alt="">
+    <a href="https://www.aatf-africa.org/">
+      <img src="uploads/images/aatf.png" alt="">
     </a>
-    <a href="#">
-      <img src="uploads/images/image1.jpg" alt="">
+    <a href="https://www.aecfafrica.org/">
+      <img src="uploads/images/acef.svg" alt="">
     </a>
-    <a href="#">
-      <img src="uploads/images/image1.jpg" alt="">
+    <a href="https://iucn.org/">
+      <img src="uploads/images/iucn.svg" alt="">
     </a>
-    <a href="#">
-      <img src="uploads/images/image1.jpg" alt="">
+    <a href="https://www.seaf.com/">
+      <img src="uploads/images/seaf.png" alt="">
+    </a>
+    <a href="https://www.usda.gov/">
+      <img src="uploads/images/usda.svg" alt="">
+    </a>
+    <a href="https://www.wfp.org/countries/kenya">
+      <img src="uploads/images/WFP.png" alt="">
+    </a>
+    <a href="https://www.pelumkenya.net/">
+      <img src="uploads/images/pelum.webp" alt="">
     </a>
   </div>
 
--- includes/services.php
+++ includes/services.php
@@ -1,24 +1,24 @@
 <div id="services" class="sized">
   <header>
     <h1>Our Services</h1>
-    <p class="font-medium">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit fuga voluptatibus temporibus quas labore obcaecati praesentium iusto at molestiae, natus aut sunt minus quaerat nobis explicabo, adipisci corrupti reiciendis dolorum?</p>
+    <p class="font-medium">SRSC was established with the core purpose of providing empirical evidence to inform policy for economic development in Liberia and the Mano River basin. Thus, the firm is committed to providing quality and insightful research to aid government, international and local nongovernmental organizations, civil societies, and private institutions in formulating and analyzing policies and providing matrices and empirical findings for effective project management and evaluation. </p>
   </header>
   <div class="grid">
     <section class="cell">
-     <h3>THE NAME OF THE SERVICE HERE</h3>
-     <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam, itaque. Ad nemo aliquid quidem, earum neque consequuntur odio! Expedita corrupti maiores sequi nesciunt ex rem quos dolore sed voluptates veritatis.</span>
+     <h3>Research</h3>
+     <span>SRSC has the requisite experts to conduct both quantitative and qualitative research. Our team of experts is experienced in conducting efficiency studies, supply and demand analysis, and estimation, market segmentation and analysis, nutrition analysis and food security analysis, impact studies, and gender analysis.</span>
     </section>
     <section class="cell">
-     <h3>THE NAME OF THE SERVICE HERE</h3>
-     <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam, itaque. Ad nemo aliquid quidem, earum neque consequuntur odio! Expedita corrupti maiores sequi nesciunt ex rem quos dolore sed voluptates veritatis.</span>
+     <h3>Survey/Data Collection and Analysis</h3>
+     <span>Our strength in conducting rigorous research is complemented by the ability to design the appropriate sampling methodology to suit the most complex survey or research study. We design a sampling strategy that ensures the result obtained from the survey is a true representation of the project's beneficiaries. </span>
     </section>
     <section class="cell">
-     <h3>THE NAME OF THE SERVICE HERE</h3>
-     <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam, itaque. Ad nemo aliquid quidem, earum neque consequuntur odio! Expedita corrupti maiores sequi nesciunt ex rem quos dolore sed voluptates veritatis.</span>
+     <h3>Training</h3>
+     <span>SRSC conducts Research Training (Research Proposal, Methodology, Qualitative and Quantitative Questionnaires Development, and Data Collection and Analysis and Results Presentation), training in the use of data collection and analysis tools (SPSS, STATA, Eviews, R, Kobo Collect, ODK, Survey CTO), and training in results validation and dissemination forums among others.</span>
     </section>
     <section class="cell">
-     <h3>THE NAME OF THE SERVICE HERE</h3>
-     <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam, itaque. Ad nemo aliquid quidem, earum neque consequuntur odio! Expedita corrupti maiores sequi nesciunt ex rem quos dolore sed voluptates veritatis.</span>
+     <h3>Finance and Business Advisory</h3>
+     <span>---</span>
     </section>
   </div>
 </div>
