e1ccbcef4daf029c45963e06ceeef2e28f82dec7
Timothy T. Joe <106974662+TimTJoe@users.noreply.github.com>
2025-03-25 12:53:11 +0000

    did client styles

--- includes/clients.php
+++ includes/clients.php
@@ -1,7 +1,9 @@
 <div id="our-clients" class="sized">
  <div class="left">
-  <h1>Who We Serve</h1>
-  <p class="font-medium padding-top">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque culpa qui repellat totam doloremque quas provident saepe vel. Vel ea, ullam voluptas et laboriosam animi molestiae itaque fuga ipsam molestias. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id veniam iusto, ut atque, nisi illo maiores ullam obcaecati consequatur quo possimus, magnam asperiores amet accusamus eligendi eum molestiae ipsa nesciunt!</p>
+  <header>
+    <h1>Who We Serve</h1>
+  </header>
+  <p class="font-medium padding-top">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque culpa qui repellat totam doloremque quas provident saepe vel. Vel ea, ullam voluptas et laboriosam animi molestiae itaque fuga ipsam molestias. nesciunt!</p>
  </div>
  <div class="right">
   <header>
