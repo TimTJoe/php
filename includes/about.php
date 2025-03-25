bbf65366c74051194082af5550013bb12e723fc8
Timothy T. Joe <106974662+TimTJoe@users.noreply.github.com>
2025-03-25 15:28:26 +0000

    added content

--- includes/about.php
+++ includes/about.php
@@ -5,7 +5,7 @@
  <section class="right">
     <h1>Who We Are</h1>
     <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit aliquid voluptatum pariatur minima cumque aperiam eveniet sapiente minus excepturi eos reprehenderit harum enim corrupti tenetur ad, numquam beatae est ratione? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, est! Eaque, molestias similique iste, cumque aliquid, voluptas magni delectus minus excepturi nam sed voluptatum recusandae facilis veritatis expedita ut doloribus?
+    The Statistical and Research Services Consortium (SRSC) is the foremost provider of research, training, and consultancy services to government and non-governmental organizations in Liberia, and in the international scene. The company was established by a group of researchers and academicians from both West and East Africa, with the aim to provide quality and rigorous research and consultancy services in economics, statistics, marketing, agriculture, and socioeconomic research. 
     </p>
   </section>
 </div>
--- includes/banner.php
+++ includes/banner.php
@@ -1,9 +1,9 @@
 <div id="home">
   <section>
     <h1>
-    Leading Professional Leadership Globally
+    The Preferred socio-economic research consultancy firm.
     </h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tincidunt magna non venenatis facilisis. Nunc eu facilisis risus, ac aliquam lorem.</p>
+    <p>With a solid background in research, SRSC has the technical knowledge in collecting data for agricultural, economic, and socioeconomic studies and projects; hence, the firm also conducts training in research methodology, project monitoring, evaluation, and data analysis.</p>
     <span class="row">
       <a href="#services" class="button primary">What We Do</a>
       <a href="#contact-us" class="button secondary">Contact Us</a>
