99c1354b621f03426537c5e7c6e73ef4fb1b3872
Timothy T. Joe <106974662+TimTJoe@users.noreply.github.com>
2025-03-26 08:27:57 +0000

    restyled services

--- includes/services.php
+++ includes/services.php
@@ -1,24 +1,29 @@
-<div id="services" class="sized">
+<div id="services">
   <header>
-    <h1>Our Services</h1>
-    <p class="font-medium">SRSC was established with the core purpose of providing empirical evidence to inform policy for economic development in Liberia and the Mano River basin. Thus, the firm is committed to providing quality and insightful research to aid government, international and local nongovernmental organizations, civil societies, and private institutions in formulating and analyzing policies and providing matrices and empirical findings for effective project management and evaluation. </p>
+    <h1>What We Do.</h1>
   </header>
-  <div class="grid">
-    <section class="cell">
-     <h3>Research</h3>
-     <span>SRSC has the requisite experts to conduct both quantitative and qualitative research. Our team of experts is experienced in conducting efficiency studies, supply and demand analysis, and estimation, market segmentation and analysis, nutrition analysis and food security analysis, impact studies, and gender analysis.</span>
+  <div class="flexgrid">
+    <section class="tile yellow">
+      <img src="uploads/images/image1.jpg" alt="">
+      <div>
+        <h1>Research</h1>
+        <p>SRSC has the requisite experts to conduct both quantitative and qualitative research. Our team of experts is experienced in conducting efficiency studies, supply and demand analysis, and estimation, market segmentation and analysis, nutrition analysis and food security analysis, impact studies, and gender analysis.</p>
+       </section>
+      </div>
+    <section class="tile blue">
+      <div>
+        <h1>Data Collection and Analysis</h1>
+        <p>Our strength in conducting rigorous research is complemented by the ability to design the appropriate sampling methodology to suit the most complex survey or research study. We design a sampling strategy that ensures the result obtained from the survey is a true representation of the project's beneficiaries. </p>
+      </div>
+      <img src="uploads/images/image2.jpg" alt="">
     </section>
-    <section class="cell">
-     <h3>Survey/Data Collection and Analysis</h3>
-     <span>Our strength in conducting rigorous research is complemented by the ability to design the appropriate sampling methodology to suit the most complex survey or research study. We design a sampling strategy that ensures the result obtained from the survey is a true representation of the project's beneficiaries. </span>
+    <section class="tile brown">
+    <img src="uploads/images/image3.jpg" alt="">
+    <div>
+      <h1>Training</h1>
+      <p>SRSC conducts Research Training (Research Proposal, Methodology, Qualitative and Quantitative Questionnaires Development, and Data Collection and Analysis and Results Presentation), training in the use of data collection and analysis tools (SPSS, STATA, Eviews, R, Kobo Collect, ODK, Survey CTO), and training in results validation and dissemination forums among others.</p>
+    </div>
     </section>
-    <section class="cell">
-     <h3>Training</h3>
-     <span>SRSC conducts Research Training (Research Proposal, Methodology, Qualitative and Quantitative Questionnaires Development, and Data Collection and Analysis and Results Presentation), training in the use of data collection and analysis tools (SPSS, STATA, Eviews, R, Kobo Collect, ODK, Survey CTO), and training in results validation and dissemination forums among others.</span>
-    </section>
-    <section class="cell">
-     <h3>Finance and Business Advisory</h3>
-     <span>---</span>
-    </section>
+    
   </div>
 </div>
