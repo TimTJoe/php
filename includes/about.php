bf9bd9f75abf6756482d3d19c171ec83bfd61c92
Timothy T. Joe <106974662+TimTJoe@users.noreply.github.com>
2025-03-26 07:48:15 +0000

    restyled about

--- includes/about.php
+++ includes/about.php
@@ -1,11 +1,11 @@
-<div id="about-us" class="sized">
-  <div class="left">
-    <img src="uploads/images/lady.jpg" alt="">
-  </div>
+<div id="about-us">
  <section class="right">
     <h1>Who We Are</h1>
     <p>
     The Statistical and Research Services Consortium (SRSC) is the foremost provider of research, training, and consultancy services to government and non-governmental organizations in Liberia, and in the international scene. The company was established by a group of researchers and academicians from both West and East Africa, with the aim to provide quality and rigorous research and consultancy services in economics, statistics, marketing, agriculture, and socioeconomic research. 
     </p>
   </section>
+  <div class="left">
+    <img src="uploads/images/lady.png" alt="">
+  </div>
 </div>
